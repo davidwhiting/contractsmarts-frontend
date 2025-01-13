7ed6b0a636dd8efeb4819b05972b289a08c0a850
David Whiting <david.g.whiting@gmail.com>
2025-01-12 22:27:52 -0600

    added delete from oci script to clean bucket

--- deploy_to_oci.sh
+++ deploy_to_oci.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 oci os object bulk-upload \
     --bucket-name "addin-hosting" \
-    --prefix "addin/v0.9/" \
-    --src-dir "dist/v0.1.1/" \
+    --prefix "addin/v1.0/" \
+    --src-dir "excel-addin/dist/" \
     --include "*" \
     --verify-checksum \
     --overwrite
